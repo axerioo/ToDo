80c5225e507aac51bb1082f5a8af20514eb5a9b2
axer <37694264+axerioo@users.noreply.github.com>
2025-03-29 17:25:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/todo/ToDo.kt
@@ -0,0 +1,18 @@
+// @file:Suppress("FunctionName")
+package com.example.todo
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun ToDoApp (modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun ToDoPreview() {
+    ToDoApp()
+}
---
+++ app/src/main/java/com/example/todo/ui/AddEditScreen.kt
@@ -0,0 +1,20 @@
+// @file:Suppress("FunctionName")
+package com.example.todo.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todo.ui.theme.ToDoTheme
+
+@Composable
+fun AddEditScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun AddEditScreenPreview() {
+    ToDoTheme {
+        AddEditScreen()
+    }
+}
